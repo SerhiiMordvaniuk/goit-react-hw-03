2c546feae8dec7513540262ba9c5a77c40a59a1d
Serhii Mordvaniuk <sm636332@gmail.com>
2024-12-19 11:00:50 +0100

    add contact styles

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import s from "./Contact.module.css";
+import { FaUserAlt } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
 
-const Contact = () => {
-  return <div></div>;
+const Contact = ({ name, number }) => {
+  return (
+    <>
+      <div className={s.item}>
+        <div className={s.contacts}>
+          <div className={s.contact}>
+            <FaUserAlt size="20" />
+            <p>{name}</p>
+          </div>
+          <div className={s.contact}>
+            <FaPhoneAlt size="20" />
+            <p>{number}</p>
+          </div>
+        </div>
+        <button className={s.btn}>Delete</button>
+      </div>
+    </>
+  );
 };
 
 export default Contact;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import s from "./ContactList.module.css";
+import Contact from "../Contact/Contact";
 
-const ContactList = () => {
-  return <div></div>;
+const ContactList = ({ contactList }) => {
+  return (
+    <>
+      <ul>
+        {contactList.map((item) => {
+          return (
+            <li key={item.id}>
+              <Contact name={item.name} number={item.number} />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default ContactList;
